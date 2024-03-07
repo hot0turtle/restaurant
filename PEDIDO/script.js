15908e6cbded0070d6a068d6da518ed6214fb3c3
hot0turtle <leofarel2015@gmail.com>
2024-03-06 23:44:33 -0300

    agora deixe nao feio

--- PEDIDO/script.js
+++ PEDIDO/script.js
@@ -1,41 +1,212 @@
-function comecar(){
- var nome = document.getElementById("exampleFormControlInput1").value;
- show(nome);
+function comecar() {
+    var nome = document.getElementById("exampleFormControlInput1").value;
+    show(nome);
 }
 
-function show(nome){
+var total = 0;
+
+function show(nome) {
     document.getElementById("nomePrint").textContent = nome;
     var div = document.getElementById("selects-div");
-    if (nome && nome.trim() !== ""){
-    div.style.display = "block";   
-    document.getElementById("exampleFormControlInput1").style.background = "none";
+    if (nome && nome.trim() !== "") {
+        div.style.display = "block";
+        document.getElementById("exampleFormControlInput1").style.background = "none";
     }
-    else{
+    else {
         document.getElementById("error").style.display = "flex";
     }
 
 }
 
-function fechar(){
-    document.getElementById("error").style.display = "none"; 
+function fechar() {
+    document.getElementById("error").style.display = "none";
     document.getElementById("exampleFormControlInput1").style.background = "pink";
 }
 
-function total(){
+function totalbt() {
     var vA = document.getElementById("selectPrato").value;
     var vS = document.getElementById("selectSobremesa").value;
     var vPF = document.getElementById("selectPratoFeito").value;
     var vSA = document.getElementById("selectSanduiche").value;
     var vB = document.getElementById("selectBebida").value;
 
-    console.log(vB)
-
    
+    var name = "";
+
+    var selectedOptions = [];
+
+    // Switch for selectPrato
+    switch (vA) {
+        case "Polenta com Queijo":
+            name = "Polenta com Queijo";
+            total += 10;
+            selectedOptions.push(name);
+            break;
+        case "Polenta Com Carne":
+            name = "Polenta Com Carne";
+            total += 19;
+            selectedOptions.push(name);
+            break;
+        case "Polenta com Molho":
+            name = "Polenta com Molho";
+            total += 8;
+            selectedOptions.push(name);
+            break;
+        case "Coxinha de frango":
+            name = "Coxinha de frango";
+            total += 12;
+            selectedOptions.push(name);
+            break;
+        case "Tiras de frango frito":
+            name = "Tiras de frango frito";
+            total += 20;
+            selectedOptions.push(name);
+            break;
+        default:
+            name = "Selecione um prato";
+    }
+
+    // Switch for selectPratoFeito
+    switch (vPF) {
+        case "Tradicional":
+            name = "Tradicional";
+            total += 22;
+            selectedOptions.push(name);
+            break;
+        case "Bife a Cavalo":
+            name = "Bife a Cavalo";
+            total += 24;
+            selectedOptions.push(name);
+            break;
+        case "Frango a milanesa":
+            name = "Frango a milanesa";
+            total += 25;
+            selectedOptions.push(name);
+            break;
+        case "frango a parmegiana":
+            name = "Frango a parmegiana";
+            total += 26;
+            selectedOptions.push(name);
+            break;
+        case "Frango grelhado":
+            name = "Frango grelhado";
+            total += 17;
+            selectedOptions.push(name);
+            break;
+        default:
+            name = "Selecione um prato feito";
+    }
+
+    // Switch for selectSanduiche
+    switch (vSA) {
+        case "Sanduiche Natural":
+            name = "Sanduiche Natural";
+            total += 14;
+            selectedOptions.push(name);
+            break;
+        case "Smash Burguer":
+            name = "Smash Burguer";
+            total += 30;
+            selectedOptions.push(name);
+            break;
+        case "Baguete recheado com bacon":
+            name = "Baguete recheado com bacon";
+            total += 33;
+            selectedOptions.push(name);
+            break;
+        case "Sopa no Pao italiano":
+            name = "Sopa no Pao italiano";
+            total += 21;
+            selectedOptions.push(name);
+            break;
+        case "Pizza de Borda":
+            name = "Pizza de Borda";
+            total += 55;
+            selectedOptions.push(name);
+            break;
+        default:
+            name = "Selecione um Sanduiche";
+    }
+
+    // Switch for selectSobremesa
+    switch (vS) {
+        case "Sorvete com frutas":
+            name = "Sorvete com frutas";
+            total += 70;
+            selectedOptions.push(name);
+            break;
+        case "Chocolate com Morango":
+            name = "Chocolate com Morango";
+            total += 1;
+            selectedOptions.push(name);
+            break;
+        case "Gelatina com Morango":
+            name = "Gelatina com Morango";
+            total += 100;
+            selectedOptions.push(name);
+            break;
+        case "200 doces mistos":
+            name = "200 doces mistos";
+            total += 130;
+            selectedOptions.push(name);
+            break;
+        case "3kg de Nutella":
+            name = "3kg de Nutella";
+            total += 200;
+            selectedOptions.push(name);
+            break;
+        default:
+            name = "Selecione uma sobremesa";
+    }
+
+    // Switch for selectBebida
+    switch (vB) {
+        case "Gin Tropical":
+            name = "Gin Tropical";
+            total += 20;
+            selectedOptions.push(name);
+            break;
+        case "Gin e licor de Morango":
+            name = "Gin e licor de Morango";
+            total += 17;
+            selectedOptions.push(name);
+            break;
+        case "Gin e Tônica":
+            name = "Gin e Tônica";
+            total += 16;
+            selectedOptions.push(name);
+            break;
+        case "Energetico Bally 2L":
+            name = "Energetico Bally 2L";
+            total += 13;
+            selectedOptions.push(name);
+            break;
+        case "Powerade":
+            name = "Powerade";
+            total += 6;
+            selectedOptions.push(name);
+            break;
+        default:
+            name = "Selecione uma bebida";
+    }
 
     document.getElementById('resultado').textContent = "R$" + total + ",00";
-
+    displaySelectedOptions(selectedOptions);
+    console.log(total)
+    console.log(typeof total)
 }
 
+
+function displaySelectedOptions(options) {
+    var listElement = document.getElementById('selectedList');
+    listElement.innerHTML = '';
+
+    for (var i = 0; i < options.length; i++) {
+        var listItem = document.createElement('li');
+        listItem.textContent = options[i];
+        listElement.appendChild(listItem);
+    }
+}
 function reset() {
     $("#selectSobremesa option").prop("selected", function () {
         // return defaultSelected property of the option
@@ -57,44 +228,14 @@ function reset() {
         // return defaultSelected property of the option
         return 0;
     });
-    
+
     total();
- }
-
-function fazerPedido() {
-
 }
 
+function fazerPedido() {
+    const date = new Date();
+    var numeroTelefone = "32"
+    var linkzap = "https://wa.me/" + numeroTelefone + "?text=Total:" + total + " " +date
 
-// <option value="0" selected>Selecione um prato</option>
-// <option data-value="10" value="Polenta com Queijo">Polenta com Queijo</option>
-// <option data-value="19" value="Polenta Com Carne">Polenta Com Carne</option>
-// <option data-value="8" value="Polenta com Molho">Polenta com Molho</option>
-// <option data-value="12" value="Coxinha de frango">Coxinha de frango</option>
-// <option data-value="20" value="Tiras de frango frito">Tiras de frango frito</option>
-
-// <option value="0" selected>Selecione um prato feito</option>
-// <option data-value="22" value="Tradicional">Tradicional</option>
-// <option data-value="24" value="Bife a Cavalo" >Bife a Cavalo</option>
-// <option data-value="25" value="Frango a milanesa">Frango a milanesa</option>
-// <option data-value="26" value="frango a parmegiana">frango a parmegiana</option>
-// <option data-value="17" value="Frango grelhado">Frango grelhado</option>
-
-// <option data-value="14" value="Sanduiche Natural">Sanduiche Natural</option>
-// <option data-value="30" value="Smash Burguer">Smash Burguer</option>
-// <option data-value="33" value="Baguete recheado com bacon">Baguete recheado com bacon</option>
-// <option data-value="21" value="Sopa no Pao italiano">Sopa no Pao italiano</option>
-// <option data-value="55" value="Pizza de Borda">Pizza de Borda</option>
-
-// <option data-value="70" value="Sorvete com frutas">Sorvete com frutas</option>
-// <option data-value="1" value="Chocolate com Morango">Chocolate com Morango</option>
-// <option data-value="100"value="Gelatina com Morango">Gelatina com Morango</option>
-// <option data-value="130" value="200 doces mistos">200 doces mistos</option>
-// <option data-value="200" value="3kg de Nutella">3kg de Nutella</option>
-
-
-// <option data-value="16" value="Gin Tropical">Gin Tropical</option>
-// <option data-value="17" value="Gin e licor de Morango">Gin e licor de Morango</option>
-// <option data-value="20" value="Gin e Tônica">Gin e Tônica</option>
-// <option data-value="13" value="Energetico Bally 2L">Energetico Bally 2L</option>
-// <option data-value="6" value="Powerade">Powerade</option>
+    window.open(linkzap,"_blank");
+}
